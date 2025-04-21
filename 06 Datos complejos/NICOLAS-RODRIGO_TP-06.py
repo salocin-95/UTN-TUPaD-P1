f60f7aac45895024982af5f5120f87f70e20cd4e
salocin-95 <renicolas7@gmail>
2025-04-20 23:36:04 -0300

    Modifica el titulo del archivo

---
+++ 06 Datos complejos/NICOLAS-RODRIGO_TP-06.py
@@ -0,0 +1,103 @@
+from math import pi
+
+# 1
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}
+
+print(precios_frutas)
+
+
+precios_frutas['Naranja'] = 1200
+precios_frutas['Manzana'] = 1500
+precios_frutas['Pera'] = 2300
+
+print(precios_frutas)
+
+# 2
+precios_frutas['Banana'] = 1330
+precios_frutas['Manzana'] = 1700
+precios_frutas['Melón'] = 2800
+
+print(precios_frutas)
+
+# 3
+lista = []
+for key in precios_frutas:
+    lista.append(key)
+print(lista)
+
+# 4
+class Persona():
+    def __init__(self, nombre, pais, edad):
+        self.nombre = nombre
+        self.pais = pais
+        self.edad = edad 
+    
+    def saludar(self):
+        print(f"Hola! Soy {self.nombre}, vivo en {self.pais} y tengo {self.edad} años.")
+        
+persona = Persona('Rodrigo', 'Argentina', 29)
+persona.saludar()
+
+# 5
+class Circulo():
+    
+    def __init__(self, radio):
+        self.radio = radio
+        
+    def calcular_area(self):
+        return round(pi * self.radio ** 2, 2)
+
+    def calcular_perimetro(self):
+        return round(2 * pi * self.radio, 2)
+    
+circulo = Circulo(25)
+print(circulo.calcular_area())
+print(circulo.calcular_perimetro())
+
+# 6
+string_a = "({[]})"
+string_b = "({[})"
+
+
+# 7
+class Cola():
+    
+    def __init__(self):
+        self.clientes = []
+    
+    def encolar(self, cliente):
+        self.clientes.append(cliente)
+        pass
+    
+    def desencolar(self):
+        self.clientes.pop()
+        pass
+    
+    def esta_vacia(self):
+        return len(self.clientes) == 0
+    
+    def mostar_cliente(self):
+        return self.clientes[-1] if not self.esta_vacia() else "No hay clientes en la cola"
+    
+cola = Cola()
+
+print(cola.clientes)
+
+# Demuestra que pasa cuando la cola esta vacia
+print(cola.mostar_cliente())
+
+cola.encolar('rodrigo')
+print(cola.mostar_cliente())
+
+cola.encolar('nicolas')
+print(cola.mostar_cliente())
+
+cola.encolar('ezequiel')
+print(cola.mostar_cliente())
+print(cola.clientes)
+
+cola.desencolar()
+print(cola.clientes)
+print(cola.mostar_cliente())
+
+# 9
